485ababb2b037bb3c93767fb89a47d343cd8f5a0
Alma <a.q.frankenstein@gmail.com>
2020-09-25 11:28:46 -0700

    change file names to match class

---
+++ lib/anagramChecker.rb
@@ -0,0 +1,56 @@
+require('pry')
+
+class AnagramChecker
+  attr_accessor(:phrase1, :phrase2)
+  attr_reader(:vowels)
+
+  def initialize(phrase1, phrase2)
+    @phrase1 = phrase1
+    @phrase2 = phrase2
+    @vowels = ["a", "e", "i", "o", "u", "y"]
+  end
+
+  def basic_anagram()
+    if @phrase1.split("").sort == @phrase2.split("").sort
+      return "they are anagrams"
+    else 
+      return "not anagrams"
+    end
+  end
+
+  def case_checker()
+    if @phrase1.downcase.split("").sort == @phrase2.downcase.split("").sort
+      return "they are anagrams"
+    else 
+      return "not anagrams"
+    end
+  end
+
+  def is_word(word_to_check)
+    word_array = word_to_check.downcase.split("")
+    if (word_array & @vowels).empty?
+      return "please enter real words"
+    else 
+      return true
+    end
+  end
+
+  def are_words()
+    if is_word(@phrase1) != true || is_word(@phrase2) != true
+      return "make sure both words have vowels"
+    else 
+      return true
+    end
+  end
+
+  def antigram_check()
+    phrase1_array = @phrase1.downcase.split("")
+    phrase2_array = @phrase2.downcase.split("")
+    if (phrase1_array & phrase2_array).empty?
+      return "These phrases are antigrams!"
+    else 
+      return false
+    end
+  end
+end
+
---
+++ spec/anagramChecker_spec.rb
@@ -0,0 +1,41 @@
+require('anagramChecker')
+require('rspec')
+
+describe(AnagramChecker) do
+ 
+  describe("#basic_anagram") do
+    it('checks whether two case-matching words are anagrams') do
+      lower_checker = AnagramChecker.new("cat", "act")
+      expect(lower_checker.basic_anagram()).to(eq("they are anagrams"))
+    end
+  end
+
+  describe("#case_checker") do
+    it('checks whether two words with different cases are anagrams') do
+      different_cases = AnagramChecker.new("Cat", "Act")
+      expect(different_cases.case_checker()).to(eq("they are anagrams"))
+    end
+  end
+
+  describe("#is_word") do
+    it('checks whether a word contains vowels') do
+      nonsense_input = AnagramChecker.new("cat", "ctt")
+      expect(nonsense_input.is_word("xxx")).to(eq("please enter real words"))
+      # expect(nonsense_input.is_word("cat")).to(eq(true))
+    end
+  end
+
+  describe("#are_words") do
+    it('checks whether both inputs contain vowels') do
+      nonsense_input = AnagramChecker.new("cat", "ctt")
+      expect(nonsense_input.are_words()).to(eq("make sure both words have vowels"))
+    end
+  end
+
+  describe("#antigram_check") do
+    it ('checks whether the two phrases are antigrams') do
+      antigrams = AnagramChecker.new("hi", "bye")
+      expect(antigrams.antigram_check()).to(eq("These phrases are antigrams!"))
+    end
+  end
+end
